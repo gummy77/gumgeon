23cfd2e2cf3ece3bf1daeb6252b56688e398d942
gummydummy77 <elizabeth.lawler2003@gmail.com>
2023-03-01 18:35:50 +1300

    got it wokring :)

--- Generators/Generators/NameGenerator.js
+++ Generators/Generators/NameGenerator.js
@@ -2,49 +2,6 @@ let fs = require('fs')
 
 var MarkovChain = {}
 
-function convertData (data){
-    var code = ""
-    count = 0;
-    switch (data[0]) {
-        case("char"):
-            switch(data[1]){
-                case("human"):
-                code = code+"000"
-                break
-                case("elf"):
-                code = code+"001";
-                break
-                case("dwarf"):
-                code = code+"002";
-                break
-                case("gnome"):
-                code = code+"003";
-                break
-            }
-            switch(data[2]){
-                case("na"):
-                code = code+"00"
-                break
-                case("ma"):
-                code = code+"01"
-                break
-                case("fe"):
-                code = code+"02"
-                break
-                case("su"):
-                code = code+"03"
-                break
-            }
-            if(data[3]) count = data[3]
-            break
-    }
-
-    
-    if(_count > 9) code = code + _count
-    else code = code + "0" + _count
-    return code
-}
-
 module.exports.InitialiseGenerator = function(){
     fs.readFile("./Generators/data/names.json", 'utf8' , (err, rawdata) => {
         if (err) return console.error(err);
@@ -55,19 +12,15 @@ module.exports.InitialiseGenerator = function(){
 }
 
 function generateChains(data){
-    var typeint = 0
     Object.keys(data).forEach((type) => {
-        MarkovChain[typeint.toString()] = {}
-        var subtypeint = 0
+        MarkovChain[type] = {}
+
         Object.keys(data[type]).forEach(function(subtype) {
 
             var chain = generateChain(data[type][subtype])
-            MarkovChain[typeint.toString()][subtypeint.toString()] = chain
+            MarkovChain[type][subtype] = chain
 
-            
-            subtypeint ++
         })
-        typeint ++
     })
 }
 
@@ -97,75 +50,65 @@ function generateChain(names){
     return dict_of_names;
 }
 
-exports.GenerateName = function (data) {
-    var type = (parseInt(data.slice(0, 3))).toString()
-    var subtype = (parseInt(data.slice(3, 5))).toString()
-    var count = (parseInt(data.slice(5,8))).toString()
-    var seed = parseInt(data)
+exports.GenerateName = function (_data) {
+    if(_data.gentype != "name") {
+        console.error("non-name data passed to name generator");
+        return "error";
+    }
+    // console.log("Name Generation Requested!");
+    // console.log(_data);
 
-    return generateName(type, subtype, count, seed);
+    return generateName(_data);
 }
 
-function generateName (data) {
-    
-//_type, _subtype, _count, _seed
+function generateName (_data) {
+    var type = _data.type
+    var subtype = _data.subtype
+    var count = _data.count
+    //var seed = _data.seed
 
-    var o_subtype = _subtype;
-    var dict_of_names = []
-    if(_subtype == "0" ){
-        Object.keys(MarkovChain[_type]).forEach(function(subtype) {
-            dict_of_names = joinChains(dict_of_names, MarkovChain[_type][subtype])
-        })
-    }else{
-        _subtype = (parseInt(_subtype)-1).toString()
-        dict_of_names = MarkovChain[_type][_subtype]
-    }
+    var dict_of_names = [] //not sure what any of this is for? probably needs a refactor
+    // if(subtype == "na" ){
+    //         dict_of_names = joinChains(MarkovChain[type])
+    // }else{
+        dict_of_names = MarkovChain[type][subtype]
+    //}
 
-    var combination = "___";
-    var next_letter = "";
-    var result = "";
-
-    while(true){
-        var number_of_letters = dict_of_names[combination].length;
-
-        var index = Math.round(Math.random()*(number_of_letters - 1));
-        next_letter = dict_of_names[combination][index];
-        if(next_letter == "_"){
-            break
-        } else {
-            result = result + next_letter;
-            combination = combination[1]+ combination[2] + next_letter;
-        }
-    }
-
-    if(_count <= 1) return result;
+    //console.log(`producing ${count} names with types: ${type}, ${subtype}`);
 
     var names = [];
-    names[0] = result;
+    while(names.length < count){
+        var combination = "___";
+        var next_letter = "";
+        var result = "";
 
-    for(var i = 0; i < _count-1; i++){
-        var newName = generateName(_type, o_subtype, 0, _seed+1);
-        while(names.includes(newName)){
-            var newName = generateName(_type, o_subtype, 0, _seed+1);
+        while(true){
+            var number_of_letters = dict_of_names[combination].length;
+
+            var index = Math.round(Math.random()*(number_of_letters - 1));
+            next_letter = dict_of_names[combination][index];
+            if(next_letter == "_"){
+                break
+            } else {
+                result = result + next_letter;
+                combination = combination[1]+ combination[2] + next_letter;
+            }
         }
-       names.push(newName);
+            
+        if(!names.includes(result)){
+            names.push(result);
+        }
     }
-
     return names;
 }
 
-function joinChains(obj1, obj2){
-    for (var key in obj2){
-        
-        if(obj2.hasOwnProperty(key)){
-            if(obj1.hasOwnProperty(key)){
-                obj1[key] = obj1[key].concat(obj2[key])
-            }else{
-                obj1[key] = obj2[key];
-            }
-        }
 
-    }
-
-    return obj1;
+function joinChains(obj){ //needs to be fixed for mixed names?
+    newobj = []
+    obj.forEach((subobj) => {
+         newobj = newobj.concat(subobj)
+    })
+    console.log(obj)
+    console.log(newobj)
+    return newobj;
 }
--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 const express = require('express');
-const generatorHandler = require('./Generators/GeneratorHandler.js')
+const generatorHandler = require('./Generators/GeneratorHandler.js');
 
 const port = 3000;
 
 var app = express();
 
 app.listen(port, () => {
-    generatorHandler.InitialiseGenerators()
+    generatorHandler.InitialiseGenerators();
     console.log(`listening at http://localhost:${port}`);
 })
 app.use('/static', express.static('static'))
     .get('/', (req, res) => {res.sendFile(__dirname + '/Pages/home.html')})
     .get('/sussy', (req, res) => {res.sendFile(__dirname + '/Pages/sussy.html')})
     .get('/rpgthings', (req, res) => {res.sendFile(__dirname + '/Pages/rpgthings.html')})
-    .get('/namegen', (req, res) => {res.sendFile(__dirname + '/Pages/rpgthings/name_generator.html')})
+    .get('/namegen', (req, res) => {res.sendFile(__dirname + '/Pages/rpgthings/name_generator.html')});
 
 app.route('/api')
 .get((req, res) => {
-    var type = req.query.t;
-    var data = [req.query.st, req.query.q1, req.query.q2];
-    console.log(req.query)
-    var result = generatorHandler.GenerateFrom(type, data);
+    var data = req.query;
+    var result = generatorHandler.GenerateFrom(data);
+    if(result == "error") return res.sendStatus(500);
     res.send(result);
 });
 
-app.use((req, res) => {
-    res.status(404);
-    res.sendFile(__dirname+'/Pages/404.html');
-});
+// app.use((req, res) => {
+//     console.log("someone tried to access: ");
+//     res.status(404);
+//     res.sendFile(__dirname+'/Pages/404.html');
+// });
 
 
 
